838f8f574db75b58a6885653e05afbc5f49062e7
97253 <sygw6457@gmail.com>
2023-06-01 22:14:55 +0300

    commiting

--- st_pipeline.c
+++ st_pipeline.c
@@ -19,10 +19,10 @@ AObject *ao_3 = NULL;
  * with the help of .rand
  * He will transfer one by one to the AO after him, with differences of one thousandth of a second - (1)
  */
-int opreation1(void *data)
+int opreation1(void *value)
 {
 
-    int *num = (int *)data;
+    int *num = (int *)value;
     *num = rand() % 900000 + 100000; // random
     queue_push(get_Queue(ao_1), num);
     usleep(1000000); // Sleep for 1 second (1,000,000 microseconds)
@@ -33,12 +33,13 @@ int opreation1(void *data)
  * will print the number, check if the number is prime, and print true or false accordingly.
  * will add to number 11 and transfer it to the next AO
  */
-int opreation2(void *data)
+int opreation2(void *value)
 {
-    int *num = (int *)data;
-    int value = *num;
+    int *num = (int *)value;
+    int data = *num;
 
-    printf("%d\n", value);
+    printf("%d", data);
+    printf("\n");
 
     if (isPrime(*num))
     {
@@ -57,12 +58,13 @@ int opreation2(void *data)
 
 /* like operation 2 just decrease 13 */
 
-int opreation3(void *data)
+int opreation3(void *value)
 {
-    int *num = (int *)data;
-    int value = *num;
+    int *num = (int *)value;
+    int data = *num;
 
-    printf("%d\n", value);
+    printf("%d", data);
+    printf("\n");
 
     if (isPrime(*num))
     {
@@ -83,9 +85,9 @@ int opreation3(void *data)
  * The fourth AO will print the received number, add to the number 2, and print the new number.
  * If you did everything correctly, the last number will be equal to the first
  */
-int opreation4(void *data)
+int opreation4(void *value)
 {
-    int *num = (int *)data;
+    int *num = (int *)value;
     printf("%d\n", *num);
     *num += 2;
     printf("%d\n", *num);
@@ -94,9 +96,11 @@ int opreation4(void *data)
 
 int main(int argc, char *argv[])
 {
+    // parameter's for random
     int N;
     long seed;
 
+    // the usage format 
     if (argc < 2 || argc > 3)
     {
         printf("Usage: pipeline N [seed]\n");
@@ -111,7 +115,7 @@ int main(int argc, char *argv[])
     seed = time(NULL);
     srand(seed);
 
-    // Create the ActiveObjects
+    // Create the ActiveObjects for each opreation init and modify thim
     ao_0 = createAObject(opreation1);
     ao_1 = createAObject(opreation2);
     ao_2 = createAObject(opreation3);
@@ -126,9 +130,9 @@ int main(int argc, char *argv[])
             printf("Memory allocation failed.\n");
             return 1;
         }
-
+        // push to queue what you extract from the Active object and modify the relevant data
         queue_push(get_Queue(ao_0), num);
-        sleep(1);
+        usleep(1000000); // sleep for 1 sec what ineeded in task read the readme.md
         printf("\n");
 
         i++;
@@ -139,6 +143,7 @@ int main(int argc, char *argv[])
     queue_push(get_Queue(ao_2), NULL);
     queue_push(get_Queue(ao_3), NULL);
 
+    // stop all other objects after its done
     stopAObject(ao_0);
     stopAObject(ao_1);
     stopAObject(ao_2);
